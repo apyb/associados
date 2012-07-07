282d2373d97631a303849d4790bfee0d7aa52b2a
Valder Gallo <valdergallo@gmail.com>
2012-07-06 23:33:18 -0300

    add user profile for members

---
+++ app/auth/models.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from django.db import models
+from app.core.models import DefaultFields
+from django.contrib.localflavor.br.br_states import STATE_CHOICES
+from django.utils.translation import ugettext as _
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+
+class Organization(DefaultFields):
+    name = models.CharField(_('Name'), max_length=250)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __unicode__(self):
+        return self.name
+
+
+class City(DefaultFields):
+    state = models.CharField(_('State'), max_length=2, choices=STATE_CHOICES)
+    name = models.CharField(_('Name'), max_length=250)
+
+    class Meta:
+        ordering = ('state', 'name')
+
+    def __unicode(self):
+        return self.name
+
+
+def get_public_key_storage_path(instance, filename):
+        return '%s/public_key/%Y/%m/%d/%s/' % (settings.MEDIA_ROOT, instance.pk)
+
+
+class UserProfile(DefaultFields):
+    CATEGORY_CHOICE = ((1, _('Student')),
+                       (2, _('Member')))
+
+    user = models.OneToOneField(User)
+    organization = models.ForeignKey(Organization, null=True)
+    cpf = models.CharField(_('CPF'), max_length=11, db_index=True, null=True)
+    fone = models.CharField(_('Fone'), max_length=50, null=True)
+    address = models.TextField(_('Address'), null=True)
+    city = models.ForeignKey(City, db_index=True, null=True)
+    public_key = models.FileField(_('Public Key'), upload_to=get_public_key_storage_path,
+                                  null=True)
+    category = models.CharField(_('Category'), max_length=1, choices=CATEGORY_CHOICE,
+                                db_index=True, null=True)
+    relation_with_community = models.TextField(_('Relation with community'), null=True, blank=True)
+    malling = models.BooleanField(_('Malling'), default=True)
+    partner = models.BooleanField(_('Partner'), default=True)
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
