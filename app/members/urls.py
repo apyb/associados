b5e68d44ee116a052ba42c9a677c8b12e6f4fe1e
Marcos Daniel Petry <marcospetry@gmail.com>
2012-07-09 22:02:05 -0300

    fix url namming

--- app/members/urls.py
+++ app/members/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, url
 from app.members.views import MemberListView, register
 
 urlpatterns = patterns('',
-    url(r'^register/$', register, name='member-register'),
+    url(r'^register/$', register, name='people-member-register'),
     url(r'^list/$', MemberListView.as_view(), name='people-members-list'),
 )
 
