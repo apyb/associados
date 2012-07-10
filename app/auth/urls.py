3da38f1b997250f78dd5d57e79fbbaa3492e8539
Marcos Daniel Petry <marcospetry@gmail.com>
2012-07-09 22:03:10 -0300

    removed old app files

--- app/auth/urls.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-from django.conf.urls import patterns, url
-from app.auth.views import register, UserListView
-
-urlpatterns = patterns('',
-    url(r'^register/$', register, name='member-register'),
-    url(r'^list/$', UserListView.as_view(), name='people-members-list'),
-)
