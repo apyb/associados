568c0f2ab10f1c786371dc4d23d5932cb5ca80fb
Marcos Daniel Petry <marcospetry@gmail.com>
2012-07-18 22:52:34 -0300

    added payment admin

---
+++ app/payment/tests/test_admin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django.contrib.flatpages.models import FlatPage
+from django.test import TestCase
+from django.contrib import admin as django_admin
+from app.payment.models import PaymentType
+
+class PaymentAdminTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_PaymentType_model_should_be_registered_within_the_admin(self):
+        self.assertIn(PaymentType, django_admin.site._registry)
+
