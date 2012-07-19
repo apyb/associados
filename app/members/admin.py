32b9979e7aa2224573a8daae8bc4e826067cba09
Marcos Daniel Petry <marcospetry@gmail.com>
2012-07-18 22:53:26 -0300

    added Category

--- app/members/admin.py
+++ app/members/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from app.members.models import Category
 from models import Member, Organization, City
 
 admin.site.register(Member)
+admin.site.register(Category)
 admin.site.register(Organization)
 admin.site.register(City)
