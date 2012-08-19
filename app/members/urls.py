90e9b9e3685b53e476ed806164fd5dcd627dab2c
Marcos Daniel Petry <marcospetry@gmail.com>
2012-08-19 12:45:47 -0300

    added signup view

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, url
-from app.members.views import MemberListView, register, member_form, dashboard
+from app.members.views import MemberListView, register, member_form, dashboard, SignupView
 
 urlpatterns = patterns('',
+    url(r'^signup/$', SignupView.as_view(), name='members-signup'),
     url(r'^register/$', register, name='member-register'),
     url(r'^list/$', MemberListView.as_view(), name='members-list'),
     url(r'^change/$', member_form, name='members-form'),  # may it should be /update/
-    url(r'^dashboard/$', dashboard, name='members-form'),
+    url(r'^dashboard/$', dashboard, name='members-dashboard'),
 )
--- app/members/views.py
+++ app/members/views.py
@@ -1,8 +1,11 @@
 # encoding: utf-8
-from datetime import datetime
+from httplib import HTTPResponse
+from django.contrib.auth import login, authenticate
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.utils.safestring import SafeUnicode
 
 from django.db.models import Q
@@ -13,26 +16,12 @@ from django.contrib import messages
 
 from app.members.models import Member
 from app.members.forms import MemberForm, UserForm, UserEditionForm
+from django.views.generic.edit import FormView
+from django.core.urlresolvers import reverse_lazy
 
 
-def register(request):
-    member_form = MemberForm(request.POST or None)
-    user_form = UserForm(request.POST or None)
 
-    if request.method == 'POST' and user_form.is_valid() and member_form.is_valid():
-        user = user_form.save()
-        member = member_form.save(user)
-        login = reverse('auth-login')
-        messages.add_message(request, messages.INFO, SafeUnicode('Seu regitro foi realizado com sucesso. </br><a href="%s">Acesse seu perfil, para terminar de prencher se cadastro.</a>' % (login)))
-        #return HttpResponseRedirect(reverse('payment', kwargs={'member_id': member.id}))
-    else:
-        messages.add_message(request, messages.ERROR, 'Houve um problema no seu cadastro. Verifique os campos abaixo')
-    return render(request,
-        'members/member_register.html',
-            {
-            'user_form': user_form,
-            'member_form': member_form,
-            })
+
 
 
 class MemberListView(ListView):
@@ -59,33 +48,83 @@ class MemberListView(ListView):
         return context
 
 
+class SignupView(FormView):
+    template_name = 'members/member_signup.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('members-dashboard')
+
+    def form_valid(self, form):
+        form.save()
+        user = authenticate(
+            username=self.request.POST['username'],
+            password=self.request.POST['password1']
+        )
+        login(self.request, user)
+        messages.success(self.request, 'Você está cadastrado! Complete os seus dados para\
+         prosseguir com o registro na associação!')
+        return super(SignupView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Houve um erro ao cadastrar-se')
+        return super(SignupView, self).form_invalid(form)
+
+
 @login_required
 def member_form(request):
-    member = Member.objects.get(user=request.user)
+    try:
+        member = Member.objects.get(user=request.user)
+    except Member.DoesNotExist:
+        member = Member()
     user_form = UserEditionForm(request.POST or None, instance=request.user)
     member_form = MemberForm(request.POST or None, instance=member)
-
     if request.POST:
         if member_form.is_valid() and user_form.is_valid():
             member_form.save(user=request.user)
             user_form.save()
             messages.add_message(request, messages.INFO, 'Seus dados foram atualizados com sucesso')
+            return HttpResponseRedirect(reverse('members-dashboard'))
         else:
-            messages.add_message(request, messages.INFO, 'Ocorreu um erro ao tentar salvar seus dados. verifique o form abaixo.')
+            messages.add_message(request, messages.ERROR, 'Ocorreu um erro ao tentar salvar seus dados. verifique o form abaixo.')
 
-    return render(request, "members/member_form.html",
-        {"member_form": member_form,
-        'user_form': user_form}
-        )
+    return render(request,
+        "members/member_form.html",{
+            "member_form": member_form,
+            'user_form': user_form
+        }
+    )
 
 
 @login_required
 def dashboard(request):
     #verifica pagamento ainda valido
-    payment_results = request.user.member.get_payment_check_list()
+    try:
+        payment_results = request.user.member.get_payment_check_list()
+    except Member.DoesNotExist:
+        return HttpResponseRedirect(reverse('members-form'))
+
     return render(request, "members/dashboard.html",
         {"expired": payment_results['expired'],
         "last_payment": payment_results['last_date'],
         "days_left": payment_results['days_left']
          }
         )
+
+def register(request):
+    member_form = MemberForm(request.POST or None)
+    user_form = UserForm(request.POST or None)
+
+    if request.method == 'POST':
+        if user_form.is_valid() and member_form.is_valid():
+            user = user_form.save()
+            member = member_form.save(user)
+            login = reverse('auth-login')
+            messages.add_message(request, messages.INFO, SafeUnicode('Seu regitro foi realizado com sucesso. </br><a href="%s">Acesse seu perfil, para terminar de prencher se cadastro.</a>' % (login)))
+        else:
+            messages.add_message(request, messages.ERROR, 'Houve um problema no seu cadastro. Verifique os campos abaixo')
+    return render(request,
+        'members/member_register.html',{
+            'user_form': user_form,
+            'member_form': member_form
+        }
+    )
+
