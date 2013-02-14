8d26ac4f17fd0b3dcb37bda6408bceff5332b114
Marcos Daniel Petry <marcospetry@gmail.com>
2013-02-14 02:41:11 -0200

    fix default datetime field

--- app/payment/models.py
+++ app/payment/models.py
@@ -1,7 +1,7 @@
+from datetime import datetime
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
-from django.utils import timezone
 from app.members.models import Member, Category
 
 
@@ -42,7 +42,7 @@ class Payment(models.Model):
 
 class Transaction(models.Model):
     payment = models.ForeignKey(Payment)
-    date = models.DateTimeField(default=timezone.now().date())
+    date = models.DateTimeField(default=datetime.now())
     code = models.CharField(max_length=50)
     status = models.CharField(max_length=1, choices=TRANSACTION_STATUS)
     price = models.DecimalField(max_digits=5, decimal_places=2)
