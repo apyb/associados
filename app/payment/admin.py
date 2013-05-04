135cdb25316545763b76f0e24f20744d82fe40a0
Osvaldo Santana Neto <osantana@gmail.com>
2013-05-03 21:41:44 -0300

    Some code cleanup and fixes #75 and returning better error codes

--- app/payment/admin.py
+++ app/payment/admin.py
@@ -1,30 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
 from django.contrib import admin
-from app.payment.models import Payment, Transaction, PaymentType
 from django.utils.translation import gettext_lazy as _
+
+from app.payment.models import Payment, Transaction, PaymentType
 
 
 class TransactionInline(admin.StackedInline):
     model = Transaction
-    readonly_fields = ('date', 'code', 'status', 'price')
     extra = 0
-
-    def has_delete_permission(self, request, obj=None):
-        return False
 
     def has_add_permission(self, request):
         return False
+
 
 def make_paid(modeladmin, request, queryset):
     for payment in queryset:
         if payment.last_transaction and payment.last_transaction.status != 3:
             payment.last_transaction.status = 3
             payment.last_transaction.save()
+
+
 make_paid.short_description = _("Set last transactions as paid")
+
 
 def last_transaction_name(obj):
     return obj.last_transaction.get_status_display()
-last_transaction_name.short_description = _('Last Transaction')
 
+
+last_transaction_name.short_description = _('Last Transaction')
 
 
 class PaymentAdmin(admin.ModelAdmin):
@@ -41,6 +46,7 @@ class PaymentAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, request):
         return False
+
 
 admin.site.register(Payment, PaymentAdmin)
 admin.site.register(PaymentType)
