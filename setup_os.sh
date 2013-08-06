b7b59836321622e89bfdab56115f06ef5b7dc137
Fernando Macedo <fgmacedo@gmail.com>
2013-08-05 21:28:59 -0300

    setup cleanup with requirements hierarchy

--- setup_os.sh
+++ setup_os.sh
@@ -3,13 +3,11 @@
 if [[ $OSTYPE == 'linux-gnu' ]]; then
     echo 'Linux: Instalando dependencias...'
     sleep 3
-    pip install -r requirements.txt
     pip install -r requirements_test.txt
 
 elif [[ $OSTYPE == darwin* ]]; then
     echo 'OSX: Instalando dependencias...'
     sleep 3
-    pip install -r requirements.txt
     pip install -r requirements_test_osx.txt
 fi
 
