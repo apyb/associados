da9586a00ed844840ff5349ddfb05232b60674e5
Renato Oliveira <renatooliveira.cin@gmail.com>
2014-06-23 23:49:58 -0300

    Added url to verify-membership

--- associados/urls.py
+++ associados/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns(
     '',
     url(r'^members/', include('app.members.urls')),
     url(r'^payment/', include('app.payment.urls')),
+    url(r'^api/', include('app.api.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
