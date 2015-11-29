e590b4b604b2b5ddb23f56f5b2b69fc9ad3f1458
Carlos Leite <carlos.leite@znc.com.br>
2015-11-29 08:44:12 -0200

    debug-toolbar should be a personal choice

--- associados/settings_local_model.py
+++ associados/settings_local_model.py
@@ -29,28 +29,28 @@ COMPRESS_PRECOMPILERS = (
     ('text/less', '/opt/local/lib/node_modules/less/bin/lessc {infile} {outfile}'),
 )
 
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False
-}
+# DEBUG_TOOLBAR_CONFIG = {
+#     'INTERCEPT_REDIRECTS': False
+# }
 
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-)
+# DEBUG_TOOLBAR_PANELS = (
+#     'debug_toolbar.panels.timer.TimerDebugPanel',
+#     'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+#     'debug_toolbar.panels.template.TemplateDebugPanel',
+#     'debug_toolbar.panels.sql.SQLDebugPanel',
+#     'debug_toolbar.panels.signals.SignalDebugPanel',
+#     'debug_toolbar.panels.logger.LoggingPanel',
+# )
 
 MIDDLEWARE_CLASSES = list(MIDDLEWARE_CLASSES)
-MIDDLEWARE_CLASSES.append(
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
-)
+# MIDDLEWARE_CLASSES.append(
+#     'debug_toolbar.middleware.DebugToolbarMiddleware'
+# )
 
 INSTALLED_APPS = list(INSTALLED_APPS)
-INSTALLED_APPS.append(
-    'debug_toolbar'
-)
+# INSTALLED_APPS.append(
+#     'debug_toolbar'
+# )
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_FILE_PATH = '/tmp/lead-messages'  # change this to a proper location
