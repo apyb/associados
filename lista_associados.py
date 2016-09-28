31b11b0465b8dba45b7e22b6a7cfc77d1701aa2a
Filipe Cifali <cifali.filipe@gmail.com>
2016-09-28 14:21:38 -0300

    Gerar a lista de associados validos

--- lista_associados.py
+++ lista_associados.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
-#  dict(nome="", email="", tipo="Profissional", data=DateTime(""), paga=True, renovada=DateTime("")),
-from django.utils.timezone import datetime
 
+from app.payment.models import Payment
+import datetime
 
-class DateTime(object):
-    def __init__(self, date):
-        try:
-            self.date = datetime.strptime(date, "%Y-%m-%d")
-        except ValueError:
-            self.date = datetime.strptime(date, "%d/%m/%Y")
+cyear = datetime.now().year
+lyear = cyear - 1
+month = datetime.now().month
+day = datetime.now().day
 
-
-lista = [
-
-]
-
-
+payments = Payment.objects.filter(date__range=(datetime.date(lyear, month, day), datetime.date(cyear, month, day))).order_by('type')
+for p in payments:
+    print p.member, p.date.strftime('%Y-%m-%d'), p.valid_until.strftime('%Y-%m-%d'), p.type.category
