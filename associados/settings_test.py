9040f98cd975b0d2e4e2814f173b7244568626f1
Patrick Mazulo <pmazulo@gmail.com>
2020-05-28 09:40:11 -0300

    Remove django_nose

--- associados/settings_test.py
+++ associados/settings_test.py
@@ -3,16 +3,6 @@ from .settings import *
 
 DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3', 'NAME': '::memory::'}
 
-INSTALLED_APPS = list(INSTALLED_APPS)
-INSTALLED_APPS.append(
-    'django_nose'
-)
-INSTALLED_APPS.append(
-    'nose'
-)
-
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 LANGUAGE_CODE = 'pt_BR'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
